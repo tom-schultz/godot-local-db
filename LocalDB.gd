e7eea7370ec2323399b712bb8f9c24a31ae516c9
Tom Schultz <2112416+tom-schultz@users.noreply.github.com>
2023-09-23 22:57:16 -0400

    Cleanup, static constructor.

--- LocalDB.gd
+++ LocalDB.gd
@@ -1,52 +1,41 @@
 extends Node
 class_name LocalDB
 
-const db_path : String = "user://local_db.json"
+const DB_PATH : String = "user://local_db.json"
 
-static var _initialized : bool
 static var _data : Dictionary
 
-static func has_key(key: String):
-	_initialize()
-	assert(_data != null, "_data is null!")
-	return _data.has(key)
-
-static func read_key(key: String):
-	_initialize()
-	assert(_data != null, "_data is null!")
-	assert(_data.has(key), "_data doesn't have %s!" % key)
-	return _data[key]
-
-static func write_key(key: String, value):
-	_initialize()
-	_data[key] = value
-	_write_save()
-
 static func delete_key(key: String):
-	_initialize()
 	_data.erase(key)
 	_write_save()
 
+static func has_key(key: String):
+	return _data.has(key)
+
+static func read_key(key: String):
+	return _data[key] if _data.has(key) else null
+
+static func write_key(key: String, value):
+	_data[key] = value
+	_write_save()
+
+static func _static_init():
+	assert(DB_PATH.is_absolute_path())
+
+	if not FileAccess.file_exists(DB_PATH):
+		_data = Dictionary()
+		return
+
+	var db_file = FileAccess.open(DB_PATH, FileAccess.READ)
+	_data = JSON.parse_string(db_file.get_as_text())
+	assert(_data != null, "Couldn't load file: %s" % db_file.get_path_absolute())
+
 static func _write_save():
 	var json = JSON.stringify(_data)
-	
+
 	assert(json != null,
 			"Could not serialize _data to JSON!\n_data: %s" \
 			% [_data])
-	
-	var db_file = FileAccess.open(db_path, FileAccess.WRITE)
-	db_file.store_string(json)
 
-static func _initialize():
-	if _initialized:
-		return
-	
-	if not FileAccess.file_exists(db_path):
-		_data = Dictionary()
-		return
-	
-	var db_file = FileAccess.open(db_path, FileAccess.READ)
-	_data = JSON.parse_string(db_file.get_as_text())
-	
-	assert(_data != null, "Couldn't load file: %s" % db_file.get_path_absolute())
-	_initialized = true
+	var db_file = FileAccess.open(DB_PATH, FileAccess.WRITE)
+	db_file.store_string(json)
